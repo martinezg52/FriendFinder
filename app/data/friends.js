659c2c1864d4ba0b689268be3c892acef5acb69b
geo_ep <geoffrey.xwin@gmail.com>
2019-07-21 12:49:59 -0700

    adding html and friends file

---
+++ app/data/friends.js
@@ -0,0 +1,33 @@
+//Inserting friends data as objects
+
+var friendsData = [
+    {
+        name: "Bubbles",
+        photo: "https://www.google.com/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&ved=2ahUKEwjZw6CarcXjAhVKGzQIHc6HAIAQjRx6BAgBEAU&url=https%3A%2F%2Fwww.tvguide.com%2Fnews%2Ftrailer-park-boys-mike-smith-arrested-alleged-domestic-battery%2F&psig=AOvVaw1ZFycMRn7Q_CRVrIcNhApp&ust=1563775865134990",
+        scores: [3,4,5,5,2,3,4,1,5,5]
+    },
+    {
+        name: "Ricky",
+        photo: "https://www.google.com/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&ved=2ahUKEwjP-bvQu8XjAhVGvZ4KHd3mDrAQjRx6BAgBEAU&url=https%3A%2F%2Fwww.trailerparkboysmerch.com%2Fmens-t-shirts%2Ftpb-animation-ricky-sh-tmobile&psig=AOvVaw1KNhmkzg43NpgXDclfmNA4&ust=1563779696686119",
+        scores: [4,4,4,5,5,5,3,4,5,2]
+    },
+    {
+        name: "Julian",
+        photo: "https://www.google.com/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&ved=2ahUKEwj46rOXvMXjAhWQtZ4KHXTXBDEQjRx6BAgBEAU&url=https%3A%2F%2Fimgflip.com%2Fmemetemplate%2F59510630%2Fjulian-trailer-park-boys&psig=AOvVaw1hzfF0d6vTCOLB5dhRKnpz&ust=1563779829376484",
+        scores: [1,1,1,2,2,2,3,3,3,4]
+    },
+    {
+        name: "Mr Lahey",
+        photo: "https://www.google.com/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&ved=2ahUKEwiCxsHavMXjAhXO854KHZ5lA60QjRx6BAgBEAU&url=https%3A%2F%2Fwww.pinkbike.com%2Fphoto%2F6762298%2F&psig=AOvVaw3_hM8mVM0a59B8t2tBI-H0&ust=1563780012463084",
+        scores: [3,3,3,4,4,5,5,1,4,1]
+    },
+    {
+        name: "Randy BoBandy",
+        photo: "https://www.google.com/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&ved=2ahUKEwiz6trhvcXjAhXOrJ4KHf7vAwsQjRx6BAgBEAU&url=http%3A%2F%2Fbusy.org%2F%40mouradb4%2Frandy-bobandy&psig=AOvVaw30J08i11-tUUyl80sZBQMn&ust=1563780301070048",
+        scores: [5,5,5,3,1,1,4,4,5,5]
+    }
+];
+
+
+//Export to API Route
+module.exports = friendsData;
