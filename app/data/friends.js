76e72a959e5f3eaec55101daac18a9fe6fe5506e
geo_ep <geoffrey.xwin@gmail.com>
2019-07-23 00:59:22 -0700

    App now working

--- app/data/friends.js
+++ app/data/friends.js
@@ -3,28 +3,33 @@
 var friendsData = [
     {
         name: "Bubbles",
-        photo: "https://www.google.com/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&ved=2ahUKEwjZw6CarcXjAhVKGzQIHc6HAIAQjRx6BAgBEAU&url=https%3A%2F%2Fwww.tvguide.com%2Fnews%2Ftrailer-park-boys-mike-smith-arrested-alleged-domestic-battery%2F&psig=AOvVaw1ZFycMRn7Q_CRVrIcNhApp&ust=1563775865134990",
+        photo: "https://a1cf74336522e87f135f-2f21ace9a6cf0052456644b80fa06d4f.ssl.cf2.rackcdn.com/images/characters/p-trailer-park-boys-mike-smith.jpg",
         scores: [3,4,5,5,2,3,4,1,5,5]
     },
     {
         name: "Ricky",
-        photo: "https://www.google.com/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&ved=2ahUKEwjP-bvQu8XjAhVGvZ4KHd3mDrAQjRx6BAgBEAU&url=https%3A%2F%2Fwww.trailerparkboysmerch.com%2Fmens-t-shirts%2Ftpb-animation-ricky-sh-tmobile&psig=AOvVaw1KNhmkzg43NpgXDclfmNA4&ust=1563779696686119",
+        photo: "https://a1cf74336522e87f135f-2f21ace9a6cf0052456644b80fa06d4f.ssl.cf2.rackcdn.com/images/characters/p-trailer-park-boys-robb-wells.jpg",
         scores: [4,4,4,5,5,5,3,4,5,2]
     },
     {
         name: "Julian",
-        photo: "https://www.google.com/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&ved=2ahUKEwj46rOXvMXjAhWQtZ4KHXTXBDEQjRx6BAgBEAU&url=https%3A%2F%2Fimgflip.com%2Fmemetemplate%2F59510630%2Fjulian-trailer-park-boys&psig=AOvVaw1hzfF0d6vTCOLB5dhRKnpz&ust=1563779829376484",
+        photo: "https://a1cf74336522e87f135f-2f21ace9a6cf0052456644b80fa06d4f.ssl.cf2.rackcdn.com/images/characters/p-trailer-park-boys-john-paul-tremblay.jpg",
         scores: [1,1,1,2,2,2,3,3,3,4]
     },
     {
         name: "Mr Lahey",
-        photo: "https://www.google.com/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&ved=2ahUKEwiCxsHavMXjAhXO854KHZ5lA60QjRx6BAgBEAU&url=https%3A%2F%2Fwww.pinkbike.com%2Fphoto%2F6762298%2F&psig=AOvVaw3_hM8mVM0a59B8t2tBI-H0&ust=1563780012463084",
-        scores: [3,3,3,4,4,5,5,1,4,1]
+        photo: "https://the300s.files.wordpress.com/2017/10/jimlahey.jpg",
+        scores: [3,5,2,5,4,5,5,1,4,1]
     },
     {
         name: "Randy BoBandy",
-        photo: "https://www.google.com/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&ved=2ahUKEwiz6trhvcXjAhXOrJ4KHf7vAwsQjRx6BAgBEAU&url=http%3A%2F%2Fbusy.org%2F%40mouradb4%2Frandy-bobandy&psig=AOvVaw30J08i11-tUUyl80sZBQMn&ust=1563780301070048",
-        scores: [5,5,5,3,1,1,4,4,5,5]
+        photo: "https://vignette.wikia.nocookie.net/trailerpark/images/a/a0/Randy.jpg",
+        scores: [5,2,2,1,1,4,4,5,5,4]
+    },
+    {
+        name: "Lucy",
+        photo: "https://img.huffingtonpost.com/asset/5cd65af52100003500c5164f.jpeg",
+        scores: [2,2,4,4,5,5,2,1,3,2]
     }
 ];
 
---
+++ app/routing/htmlRoutes.js
@@ -0,0 +1,26 @@
+console.log('HTML Route Connected Successfully');
+
+
+// Requiring Node Path Dependency
+var path = require('path');
+
+
+// Including 2 routes, survey and home
+
+function htmlRoutes(app) {
+
+  // A GET Route to /survey which should display the survey page.
+  app.get('/survey', function (req, res) {
+    res.sendFile(path.join(__dirname + '/../public/survey.html'));
+  });
+
+  // A default USE route that leads to home.html which displays the home page.
+  app.use(function (req, res) {
+    res.sendFile(path.join(__dirname + '/../public/home.html'));
+  });
+
+}
+
+
+// Export for use in main server.js file
+module.exports = htmlRoutes;
